f3d7e98d7a1f7724e22b7c2bd378ee4732fc8e11
Nathan Young <nathancyoung@hotmail.com>
2019-03-18 19:10:28 -0400

    almost works - remove button not working

--- app.js
+++ app.js
@@ -4,37 +4,48 @@
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController',NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
+.directive('foundItems',FoundItemsDirective)
 .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+
+function FoundItemsDirective() {
+  var ddo = {
+    templateUrl: 'foundItems.html',
+    scope: {
+    items: '<',
+    onRemove: '&'
+    }
+  }
+ return ddo;
+}
 
 
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService) {
   var menu = this;
+ 
+  menu.helloWorld = function() {
+    console.log("hello miami");
+    return "hello miami beach";
+  };
 
-  var promise = MenuSearchService.getMatchedMenuItems(menu.searchItem);
+  menu.check = "Hello World";
 
   menu.doIt = function () {
 //    console.log(menu.searchItem);
+    var promise = MenuSearchService.getMatchedMenuItems(menu.searchItem);
     promise.then(function (response) {
-      var url_response = response.data.menu_items;
-      var ender = response.data.menu_items.length;
-      console.log('length', ender);
-      menu.foundItems = [];
-
-      for (var i=0.0; i< ender; i++) {
-         if (url_response[i].description.toLowerCase().indexOf(menu.searchItem.toLowerCase())>=0) {
-              menu.foundItems.push(url_response[i]);
-         };
-      };
-    })
-    .catch(function (error) {
-      console.log("Something went terribly wrong.");
+      menu.Items = response;
+      return promise;
     });
 
-  };
+ }
 
-
+menu.removeItem = function( itemIndex) {
+  console.log("I got here again");
+  menu.Items.splice(itemIndex,1);
 }
+
+};
 
 
 MenuSearchService.$inject = ['$http', 'ApiBasePath'];
@@ -42,16 +53,29 @@ function MenuSearchService($http, ApiBasePath) {
   var service = this;
 
   service.getMatchedMenuItems = function (searchItem) {
-    var response = $http({
+    var promise = $http({
       method: "GET",
       url: (ApiBasePath + "/menu_items.json")
-    });
-    console.log("This is really great",searchItem);
-    return response;
+    })
+    .then( 
+      function (response) {
+      var url_response = response.data.menu_items;
+      var ender = response.data.menu_items.length;
+      var found = [];
+      console.log(searchItem)
+      searchItem = searchItem || "";
+
+      for (var i=0.0; i< ender; i++) {
+         if (url_response[i].description.toLowerCase().indexOf(searchItem.toLowerCase())>=0) {
+                found.push(url_response[i]);
+         };
+      };
+     return found;
+      }).catch("This is not great");
+    return promise;
+
+    console.log("This is really great");
+
   };
-
-
-
-}
-
+};
 })();
